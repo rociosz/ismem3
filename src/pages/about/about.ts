68755f8b2a98db30bd836adfb4a399cbffa262b1
Rocio <rocio.sz.29@gmail.com>
2018-10-30 22:05:31 -0500

    Se agrego buscador de mapa

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,26 +1,84 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Geolocation } from '@ionic-native/geolocation';
 
+declare var google;
+
 @Component({
-  selector: 'page-about',
-  templateUrl: 'about.html'
+    selector: 'page-about',
+    templateUrl: 'about.html'
 })
 export class AboutPage {
 
-	public latitude : String;
-	public longitude : String;
+    @ViewChild('map') mapElement: ElementRef;
+    map: any;
+    marker: any;
 
-  constructor(public navCtrl: NavController, private geolocation: Geolocation) {
-  	this.geolocation.getCurrentPosition().then((resp) => {
-	 this.latitude = resp.coords.latitude
-	 this.longitude = resp.coords.longitude
-	}).catch((error) => {
-	  console.log('Error getting location', error);
-	});
-  }
+    public latitude : any;
+    public longitude : any;
 
-  
+    constructor(public navCtrl: NavController, private geolocation: Geolocation) {
+        // this.geolocation.getCurrentPosition().then((resp) => {
+        //  this.latitude = resp.coords.latitude
+        //  this.longitude = resp.coords.longitude
+        //
+        //
+        //
+        //
+        // }).catch((error) => {
+        //   console.log('Error getting location', error);
+        // });
+
+
+    }
+
+    ionViewDidLoad(){
+        this.loadMap();
+    }
+
+    loadMap(){
+
+        this.geolocation.getCurrentPosition().then((resp) => {
+
+            let latLng = new google.maps.LatLng(resp.coords.latitude, resp.coords.longitude);
+
+            let mapOptions = {
+                center: latLng,
+                zoom: 19,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            }
+
+            this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
+            this.marker = new google.maps.Marker({
+                position: latLng,
+                map: this.map,
+                title: 'Mi posicion'
+            });
+
+
+        }).catch((error) => {
+            console.log('Error getting location', error);
+        });
+
+
+        let watch = this.geolocation.watchPosition();
+        watch.subscribe((data) => {
+
+            let latLng = new google.maps.LatLng(data.coords.latitude, data.coords.longitude);
+            this.marker = new google.maps.Marker({
+                position: latLng,
+                map: this.map,
+                title: 'Current Location'
+            });
+        });
+
+
+
+
+    }
+
+
 
 
 }
